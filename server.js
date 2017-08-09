88b1fc1f093862c0c18ab12f2169a0b62b5e1368
azazel696 <cifuentes_estrada@hotmail.com>
2017-08-09 00:58:03 -0600

    fix

--- server.js
+++ server.js
@@ -14,15 +14,4 @@ var port = process.env.PORT || 5000;
 app.listen(port,() =>{
     console.log('server is running ');
 });
-//heroku
-var jsonServer = require('json-server');
-var server = jsonServer.create();
-var router = jsonServer.router('db.json');
-var middlewares = jsonServer.defaults();
-var port = Number(process.env.PORT || 3000);
-server.use(middlewares);
-server.use(router);
-server.listen(port, function () {
-  console.log('JSON Server is running')
-  
-});
+

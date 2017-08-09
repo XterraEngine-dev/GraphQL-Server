6f2ca4d478a5986ee0003039e02284066d42e288
azazel696 <cifuentes_estrada@hotmail.com>
2017-08-08 22:45:16 -0600

    Mutation

--- schema.js
+++ schema.js
@@ -108,12 +108,12 @@ const mutation = new GraphQLObjectType({
             type: CustomerType,
             args: {
                 id: { type: new GraphQLNonNull(GraphQLString) },
-                name: { type:  GraphQLString},
-                email: { type:  GraphQLString},
-                age: { type:  GraphQLInt},
+                name: { type: GraphQLString },
+                email: { type: GraphQLString },
+                age: { type: GraphQLInt },
             },
             resolve(parentValue, args) {
-                return axios.patch('http://localhost:3000/customers/'+args.id,args)
+                return axios.patch('http://localhost:3000/customers/' + args.id, args)
                     .then(res => res.data);
             }
         }

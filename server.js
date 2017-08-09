286094ea070e0071c8e8dac9099279f0caa96f1e
azazel696 <cifuentes_estrada@hotmail.com>
2017-08-09 00:51:57 -0600

    JSON SERVER

--- server.js
+++ server.js
@@ -12,6 +12,16 @@ app.use('/graphql',expressGraphQL({
 var port = process.env.PORT || 5000;
 
 app.listen(port,() =>{
-    console.log('server is running on port 4000..');
+    console.log('server is running ');
 });
 //heroku
+var jsonServer = require('json-server');
+var server = jsonServer.create();
+var router = jsonServer.router('db.json');
+var middlewares = jsonServer.defaults();
+var port = Number(process.env.PORT || 3000);
+server.use(middlewares);
+server.use(router);
+server.listen(port, function () {
+  console.log('JSON Server is running')
+});

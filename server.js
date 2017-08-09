52503232203639bd7a639429fa675140b7268627
azazel696 <cifuentes_estrada@hotmail.com>
2017-08-09 00:08:03 -0600

    port

--- server.js
+++ server.js
@@ -9,7 +9,8 @@ app.use('/graphql',expressGraphQL({
     graphiql:true
 }));
 
+var port = process.env.PORT || 8080;
 
-app.listen(4000,() =>{
+app.listen(port,() =>{
     console.log('server is running on port 4000..');
 });
